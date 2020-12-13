261396aaba898fc0145a09e88bf7c48944ffbfe4
Max Jöhnk <maxjoehnk@gmail.com>
2020-12-13 10:46:20 +0100

    feat(idle): replace xidlehook-core with user-idle
    this enables windows support and reduces complexity of the idle module

--- src/modules/idle.rs
+++ src/modules/idle.rs
@@ -1,13 +1,10 @@
 use crate::config::Config;
-use crate::modules::StateChange::Idle;
-use crate::modules::{LocalModule, StateChange};
-use futures_util::future::LocalBoxFuture;
+use crate::modules::{StateChange, Module};
+use futures_util::future::{BoxFuture};
 use futures_util::FutureExt;
 use std::time::Duration;
 use tokio::sync::mpsc::UnboundedSender;
-use xidlehook_core::modules::Xcb;
-use xidlehook_core::Timer;
-use xidlehook_core::Xidlehook;
+use user_idle::UserIdle;
 
 pub struct IdleModule {
     sender: UnboundedSender<StateChange>,
@@ -19,102 +16,25 @@ impl IdleModule {
     }
 }
 
-impl LocalModule for IdleModule {
-    fn run(&mut self, config: &Config) -> LocalBoxFuture<anyhow::Result<()>> {
+impl Module for IdleModule {
+    fn run(&mut self, config: &Config) -> BoxFuture<anyhow::Result<()>> {
         let idle_timeout = config.idle_timeout;
         let poll_rate = config.idle_poll_rate;
         async move {
             self.sender.send(StateChange::Idle(false))?;
-            let timer = OccupancyTimer {
-                sender: self.sender.clone(),
-                time: Duration::from_secs(idle_timeout),
-                urgency: Duration::from_secs(poll_rate)
-            };
-            let mut idle_hook = Xidlehook::new(vec![timer]);
-            let xcb = Xcb::new().unwrap();
-            idle_hook.main_async(&xcb).await.unwrap();
+
+            loop {
+                tokio::time::delay_for(Duration::from_secs(poll_rate)).await;
+                let idle = UserIdle::get_time().map_err(|err| anyhow::Error::msg(err.to_string()))?;
+                if idle.as_seconds() >= idle_timeout {
+                    self.sender.send(StateChange::Idle(true))?;
+                }else {
+                    self.sender.send(StateChange::Idle(false))?;
+                }
+            }
 
             Ok(())
         }
-        .boxed_local()
-    }
-}
-
-pub struct OccupancyTimer {
-    pub time: Duration,
-    pub sender: UnboundedSender<StateChange>,
-    pub urgency: Duration,
-}
-
-impl Timer for OccupancyTimer {
-    fn time_left(&mut self, idle_time: Duration) -> xidlehook_core::Result<Option<Duration>> {
-        Ok(self
-            .time
-            .checked_sub(idle_time)
-            .filter(|&d| d != Duration::default()))
-    }
-
-    fn abort_urgency(&self) -> Option<Duration> {
-        Some(self.urgency)
-    }
-
-    fn activate(&mut self) -> xidlehook_core::Result<()> {
-        self.sender.send(Idle(true))?;
-        Ok(())
-    }
-
-    fn abort(&mut self) -> xidlehook_core::Result<()> {
-        self.sender.send(Idle(false))?;
-        Ok(())
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::state::StateChange;
-    use tokio::sync::mpsc;
-
-    #[test]
-    fn abort_urgency_should_return_urgency() {
-        for secs in vec![2, 5] {
-            let timer = OccupancyTimer {
-                time: Default::default(),
-                sender: mpsc::unbounded_channel().0,
-                urgency: Duration::from_secs(secs)
-            };
-
-            let urgency = timer.abort_urgency();
-
-            assert_eq!(Some(Duration::from_secs(secs)), urgency);
-        }
-    }
-
-    #[test]
-    fn activate_should_send_idle_true() {
-        let (sender, mut receiver) = mpsc::unbounded_channel();
-        let mut timer = OccupancyTimer {
-            time: Default::default(),
-            urgency: Default::default(),
-            sender,
-        };
-
-        timer.activate().unwrap();
-
-        assert_eq!(StateChange::Idle(true), receiver.try_recv().unwrap());
-    }
-
-    #[test]
-    fn abort_should_send_idle_false() {
-        let (sender, mut receiver) = mpsc::unbounded_channel();
-        let mut timer = OccupancyTimer {
-            time: Default::default(),
-            urgency: Default::default(),
-            sender,
-        };
-
-        timer.abort().unwrap();
-
-        assert_eq!(StateChange::Idle(false), receiver.try_recv().unwrap());
+        .boxed()
     }
 }
